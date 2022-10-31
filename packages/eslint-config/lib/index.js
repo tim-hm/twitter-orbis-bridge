e874202aff342530e5bb0602fca3ef4bef4d0f2f
Tim Holmes-Mitra <tim@holmie.xyz>
2022-10-31 15:27:48 +0000

    style: add import order rule
    Signed-off-by: Tim Holmes-Mitra <tim@glimpseprotocol.io>

--- packages/eslint-config/lib/index.js
+++ packages/eslint-config/lib/index.js
@@ -1,10 +1,41 @@
 module.exports = {
+  settings: {
+    "import/internal-regex": "^@tob",
+  },
   overrides: [
     {
       files: ["*.ts"],
       parser: "@typescript-eslint/parser",
-      extends: ["prettier", "plugin:@typescript-eslint/recommended"],
-      plugins: ["@typescript-eslint"],
+      plugins: ["@typescript-eslint", "import"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+      ],
+      rules: {
+        "import/order": [
+          "warn",
+          {
+            "newlines-between": "always",
+            alphabetize: {
+              order: "asc",
+            }
+          },
+        ],
+        "eol-last": ["warn"],
+        "no-console": ["warn"],
+        "@typescript-eslint/no-unused-vars": [
+          "warn",
+          {
+            args: "none",
+          },
+        ],
+        "@typescript-eslint/explicit-function-return-type": [
+          "warn",
+          {
+            allowExpressions: true,
+          },
+        ],
+      },
     },
     {
       files: ["*.js"],
