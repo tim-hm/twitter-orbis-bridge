97cb4770b2145b815e56cd31063d246c1431c2c8
Tim Holmes-Mitra <tim@holmie.xyz>
2022-11-01 08:55:51 +0000

    chore: tidy lint and ts configs
    Signed-off-by: Tim Holmes-Mitra <tim@holmie.xyz>

--- packages/eslint-config/lib/index.js
+++ packages/eslint-config/lib/index.js
@@ -1,53 +1,42 @@
 module.exports = {
-  settings: {
-    "import/internal-regex": "^@tob",
-  },
-  overrides: [
-    {
-      files: ["*.ts"],
-      parser: "@typescript-eslint/parser",
-      plugins: ["@typescript-eslint", "import"],
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:prettier/recommended",
-      ],
-      rules: {
-        "import/order": [
-          "warn",
-          {
-            "newlines-between": "always",
-            alphabetize: {
-              order: "asc",
-            }
-          },
-        ],
-        "eol-last": ["warn"],
-        "no-console": ["warn"],
-        "@typescript-eslint/no-unused-vars": [
-          "warn",
-          {
-            args: "none",
-          },
-        ],
-        "@typescript-eslint/explicit-function-return-type": [
-          "warn",
-          {
-            allowExpressions: true,
-          },
-        ],
-      },
+    settings: {
+        "import/internal-regex": "^@tob",
     },
-    {
-      files: ["*.js"],
-      parser: "@babel/eslint-parser",
-      parserOptions: {
-        requireConfigFile: false,
-      },
-      env: {
-        browser: true,
-        jest: true,
-        node: true,
-      },
-    },
-  ],
+    ignorePatterns: ["dist/*.js"],
+    overrides: [
+        {
+            files: ["*.ts"],
+            parser: "@typescript-eslint/parser",
+            plugins: ["@typescript-eslint", "import"],
+            extends: [
+                "plugin:@typescript-eslint/recommended",
+                "plugin:prettier/recommended",
+            ],
+            rules: {
+                "import/order": [
+                    "warn",
+                    {
+                        "newlines-between": "always",
+                        alphabetize: {
+                            order: "asc",
+                        },
+                    },
+                ],
+                "eol-last": ["warn"],
+                "no-console": ["warn"],
+                "@typescript-eslint/no-unused-vars": [
+                    "warn",
+                    {
+                        args: "none",
+                    },
+                ],
+                "@typescript-eslint/explicit-function-return-type": [
+                    "warn",
+                    {
+                        allowExpressions: true,
+                    },
+                ],
+            },
+        },
+    ],
 }
